beff8a9245f4ebd85c3aa2d8dbb9fc676ea20345
devboy2011 <buituananhit.contact@gmail.com>
2026-02-05 23:20:39 +0700

    create admin routes, services

---
+++ backend/routes/admin/productRoute.py
@@ -0,0 +1,51 @@
+from fastapi import APIRouter
+from fastapi import Depends, HTTPException, Query
+import logging
+
+from models.productModel import Product
+from services.productServices import get_products, get_product_by_id
+from controllers.productController import addProductController
+from middlewares.VerifyUser import ValidateUser
+from models import authModel
+
+router = APIRouter(prefix="/api/amdin/products",tags=['Product'])
+
+logger = logging.getLogger(__name__)
+
+@router.get("/")
+def list_products(
+    page: int = Query(1, ge=1),
+    limit: int =Query(1, lte=1),
+    category: str | None = Query(None, description="Category name"),
+    q: str | None = None,
+    min_price: float | None = None,
+    max_price: float | None = None,
+    sort: str | None = None,
+    userId: str = Depends(ValidateUser(authModel.RolesEnum.admin))
+):
+    try:
+        return get_products(
+            page=page,
+            limit=limit,
+            category=category,
+            q=q,
+            min_price=min_price,
+            max_price=max_price,
+            sort=sort
+        )
+    except Exception as e:
+        logger.exception(e)
+        raise HTTPException(status_code=404, detail="Item not found")
+
+@router.get("/id/{product_id}")
+def get_product(
+    product_id: str,
+    userId: str = Depends(ValidateUser(authModel.RolesEnum.staff))
+):
+    try:
+        return get_product_by_id(
+            product_id
+        )
+    except Exception as e:
+        logger.exception(e)
+        raise HTTPException(status_code=404, detail="Item not found")
